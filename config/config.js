d9cf59579ac940d62f2ab5654aec01f450f64487
Vijayakumar <msvijayakumar1>
2023-03-01 20:38:23 -0800

    first commit

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ const connectDB = async ()=> {
     try {
         const url = process.env.MONGO_URL;
         const conn = await mongoose.connect(url, {
-            useUnifiedTopology: true,
+            useUnifiedTopology:true,
             useNewUrlParser: true,
         })
         console.log(`Mongodb Database connected ${conn.connection.host}`.bgCyan.white);
